02a54dc445f4af93cea68368ab3ed6a547ecf55e
alireza.arzehgar <alireza.arzehgar@partsoftware.com>
2025-06-09 13:15:09 +0330

    refactor(gui): remove redundant gui.ShowText function
    Removed redundant gui.ShowText function
    - Deleted cmd/api.go which used gui.ShowText
    - Updated internal/gui/gui.go to remove duplicate function
    
    Signed-off-by: alireza.arzehgar <alireza.arzehgar@partsoftware.com>

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -62,30 +62,3 @@ func (g gui) ShowText(tb TextBox) {
 func (g gui) Run() {
 	g.app.Run()
 }
-
-func ShowText(tb TextBox) error {
-	a := app.New()
-	w := a.NewWindow("Integrated Flashcard")
-	w.Resize(DefaultWindowSize)
-	w.SetFixedSize(true)
-	w.Canvas().SetOnTypedKey(func(ke *fyne.KeyEvent) {
-		if slices.Contains([]fyne.KeyName{fyne.KeyEscape, fyne.KeyQ, fyne.KeyT, fyne.KeyG}, ke.Name) {
-			w.Close()
-		}
-	})
-
-	if len(tb.Title) > MaxTitleLen {
-		tb.Title = tb.Title[:MaxTitleLen]
-	}
-
-	l := canvas.NewText(tb.Title, color.White)
-	l.TextSize = DefaultTitleSize
-	l.Alignment = fyne.TextAlignCenter
-
-	rt := widget.NewRichTextFromMarkdown(tb.Text)
-	rt.Wrapping = fyne.TextWrapBreak
-
-	w.SetContent(container.NewVBox(l, rt))
-	w.ShowAndRun()
-	return nil
-}
